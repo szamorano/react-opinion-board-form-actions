3f07879d129c2b4ee33c28befe3c9dd0661584a7
Steven Zamorano <stevenzamorano.code@gmail.com>
2026-07-25 17:47:09 -0400

    Send HTTP requests for votes via a form action.

--- src/components/Opinion.jsx
+++ src/components/Opinion.jsx
@@ -1,10 +1,17 @@
+import { use } from "react";
+import { OpinionsContext } from "../store/opinions-context";
+
 export function Opinion({ opinion: { id, title, body, userName, votes } }) {
-  function upvoteAction() {
+  const { upvoteOpinion, downvoteOpinion } = use(OpinionsContext);
+
+  async function upvoteAction() {
     console.log("UPVOTE");
+    await upvoteOpinion(id);
   }
 
-  function downvoteAction() {
+  async function downvoteAction() {
     console.log("DOWNVOTE");
+    await downvoteOpinion(id);
   }
 
   return (
