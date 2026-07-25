0cab4713d4ad4a1556b40314084cd20f7aa859ab
Steven Zamorano <stevenzamorano.code@gmail.com>
2026-07-25 15:59:45 -0400

    Added  asynchronous posting using the context

--- src/components/NewOpinion.jsx
+++ src/components/NewOpinion.jsx
@@ -1,7 +1,11 @@
+import { use } from "react";
 import { useActionState } from "react";
+import { OpinionsContext } from "../store/opinions-context";
 
 export function NewOpinion() {
-  function shareOpinionAction(prevState, formData) {
+  const { addOpinion } = use(OpinionsContext);
+
+  async function shareOpinionAction(prevState, formData) {
     const title = formData.get("title");
     const body = formData.get("body");
     const userName = formData.get("userName");
@@ -32,6 +36,7 @@ export function NewOpinion() {
     }
 
     // submit to backend
+    await addOpinion({ title, body, userName });
 
     return { errors: null };
   }
