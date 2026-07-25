aa1725967b8aceb4daf430e8c3a7469e0ab96ff9
Steven Zamorano <stevenzamorano.code@gmail.com>
2026-07-25 14:22:15 -0400

    Added the shareOpinion form action to the component.

--- src/components/NewOpinion.jsx
+++ src/components/NewOpinion.jsx
@@ -1,8 +1,42 @@
 export function NewOpinion() {
+  function shareOpinionAction(formData) {
+    const body = formData.get("body");
+    const userName = formData.get("userName");
+
+    let errors = [];
+
+    if (title.trim().length < 5) {
+      errors.push("Title must be at least five characters long");
+    }
+
+    if (body.trim().length < 10 || body.trim().length > 300) {
+      errors.push("Opinion must be between 10 and 300 characters long");
+    }
+
+    if (!userName.trim()) {
+      errors.push("Please provide your name.");
+    }
+
+    if (errors.length > 0) {
+      return {
+        errors,
+        enteredValues: {
+          title,
+          body,
+          userName,
+        },
+      };
+    }
+
+    // submit to backend
+
+    return { errors: null };
+  }
+
   return (
     <div id="new-opinion">
       <h2>Share your opinion!</h2>
-      <form>
+      <form action={shareOpinionAction}>
         <div className="control-row">
           <p className="control">
             <label htmlFor="userName">Your Name</label>
