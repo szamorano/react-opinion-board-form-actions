53e505e97825edf93dddd21c606d089fde43e947
Steven Zamorano <stevenzamorano.code@gmail.com>
2026-07-25 14:45:06 -0400

    Reuse values entered by user upon form submission.

--- src/components/NewOpinion.jsx
+++ src/components/NewOpinion.jsx
@@ -2,6 +2,7 @@ import { useActionState } from "react";
 
 export function NewOpinion() {
   function shareOpinionAction(prevState, formData) {
+    const title = formData.get("title");
     const body = formData.get("body");
     const userName = formData.get("userName");
 
@@ -46,17 +47,32 @@ export function NewOpinion() {
         <div className="control-row">
           <p className="control">
             <label htmlFor="userName">Your Name</label>
-            <input type="text" id="userName" name="userName" />
+            <input
+              type="text"
+              id="userName"
+              name="userName"
+              defaultValue={formState.enteredValues?.userName}
+            />
           </p>
 
           <p className="control">
             <label htmlFor="title">Title</label>
-            <input type="text" id="title" name="title" />
+            <input
+              type="text"
+              id="title"
+              name="title"
+              defaultValue={formState.enteredValues?.title}
+            />
           </p>
         </div>
         <p className="control">
           <label htmlFor="body">Your Opinion</label>
-          <textarea id="body" name="body" rows={5}></textarea>
+          <textarea
+            id="body"
+            name="body"
+            rows={5}
+            defaultValue={formState.enteredValues?.body}
+          ></textarea>
         </p>
 
         {formState.errors && (
